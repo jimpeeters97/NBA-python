6b9f4c5f9f20efb26a37f4641130de9f73a5b46e
jimpeeters97 <peetersjim97@gmail.com>
2020-07-11 19:04:32 +0200

    First step in GUI for fetching players in pages

--- api/ApiRequestFactory.py
+++ api/ApiRequestFactory.py
@@ -36,31 +36,35 @@ class ApiRequestFactory:
             while page is not None:
                 response = requests.get('https://www.balldontlie.io/api/v1/players?page=' + str(page) + '&per_page=100')
 
-                data = response.json()['data']
-                meta = response.json()['meta']
+                result = self.__create_players_from_response(response)
 
-                meta_dump = json.dumps(meta)
-
-                for e in data:
-                    team_dict = e.get('team')
-
-                    team = Team(team_dict.get('id'), team_dict.get('abbreviation'), team_dict.get('city'),
-                                team_dict.get('conference'), team_dict.get('division'), team_dict.get('full_name'),
-                                team_dict.get('name'))
-
-                    pl = Player(e.get('id'), e.get('first_name'), e.get('last_name'), e.get('height_feet'),
-                                e.get('height_inches'), e.get('position'), team, e.get('weight_pounds'))
-
-                    players.append(pl)
+                players.append(tuple((page, result[0])))
+                page = result[1]
 
                 time.sleep(1)
-                page = json.loads(meta_dump).get('next_page')
 
             self.set_players(players)
             return players
         else:
             logging.info('Receiving players from cache')
             return self.players
+
+    def get_players_from_page(self, page):
+        if not self.players or not (pg for pg in self.players if pg[0] == page):
+            players = []
+
+            response = requests.get('https://www.balldontlie.io/api/v1/players?page=' + str(page) + '&per_page=100')
+
+            result = self.__create_players_from_response(response)
+
+            players.append(tuple((page, result[0])))
+
+            next_page = result[1]
+
+            self.players.append(players)
+            return players, next_page
+        else:
+            return (pg for pg in self.players if pg[0] == page), (pg[0] for pg in self.players if pg[0] == (page + 1))
 
     def set_players(self, players):
         self.players = players
@@ -80,7 +84,7 @@ class ApiRequestFactory:
                 meta_dump = json.dumps(meta)
 
                 for e in data:
-                    team = Team(e.get('id'), e.get('abbreviation'), e .get('city'), e.get('conference'),
+                    team = Team(e.get('id'), e.get('abbreviation'), e.get('city'), e.get('conference'),
                                 e.get('division'), e.get('full_name'), e.get('name'))
 
                     teams.append(team)
@@ -97,3 +101,28 @@ class ApiRequestFactory:
 
     def set_teams(self, teams):
         self.teams = teams
+
+    @staticmethod
+    def __create_players_from_response(response):
+        players = []
+
+        data = response.json()['data']
+        meta = response.json()['meta']
+
+        meta_dump = json.dumps(meta)
+
+        for e in data:
+            team_dict = e.get('team')
+
+            team = Team(team_dict.get('id'), team_dict.get('abbreviation'), team_dict.get('city'),
+                        team_dict.get('conference'), team_dict.get('division'), team_dict.get('full_name'),
+                        team_dict.get('name'))
+
+            pl = Player(e.get('id'), e.get('first_name'), e.get('last_name'), e.get('height_feet'),
+                        e.get('height_inches'), e.get('position'), team, e.get('weight_pounds'))
+
+            players.append(pl)
+
+        page = json.loads(meta_dump).get('next_page')
+
+        return players, page
--- gui/PlayersWidget.py
+++ gui/PlayersWidget.py
@@ -1,4 +1,6 @@
-from PySide2.QtWidgets import (QLabel, QVBoxLayout, QWidget)
+from PySide2.QtWidgets import (QLabel, QVBoxLayout, QScrollArea, QWidget)
+from PySide2.QtCore import (Qt)
+from PySide2.QtGui import (QPalette)
 
 from api.ApiRequestFactory import ApiRequestFactory
 
@@ -8,13 +10,30 @@ class PlayersWidget(QWidget):
         factory = ApiRequestFactory.get_instance()
         QWidget.__init__(self)
 
-        players = factory.get_players()
+        page_list = factory.get_players_from_page(1)
 
         self.layout = QVBoxLayout()
 
-        for pl in players:
-            player_lb = QLabel(text=pl.first_name)
+        label_widget = QWidget()
+        label_widget.layout = QVBoxLayout()
+        for page_obj in page_list[0]:
+            for pl in page_obj[1]:
+                lb_text = pl.first_name + ' ' + pl.last_name
 
-            self.layout.addWidget(player_lb)
+                if pl.position is not None and (pl.position is not '' or pl.position is not ' '):
+                    lb_text = lb_text + ': ' + pl.position
+
+                player_lb = QLabel(text=lb_text)
+                label_widget.layout.addWidget(player_lb)
+
+        label_widget.setLayout(label_widget.layout)
+
+        self.scroll_widget = QScrollArea()
+        self.scroll_widget.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self.scroll_widget.setBackgroundRole(QPalette.Light)
+        self.scroll_widget.setWidgetResizable(True)
+
+        self.scroll_widget.setWidget(label_widget)
+        self.layout.addWidget(self.scroll_widget)
 
         self.setLayout(self.layout)
